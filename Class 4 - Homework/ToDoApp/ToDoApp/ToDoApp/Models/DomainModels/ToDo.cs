aee5f2db471592b64111ca443bd1f4d74249b478
natalijaN <45513903+natalijaN@users.noreply.github.com>
2019-06-10 23:25:04 +0200

    Commit Class 4 Homework

---
+++ Class 4 - Homework/ToDoApp/ToDoApp/ToDoApp/Models/DomainModels/ToDo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models.DomainModels
+{
+    public enum ImporanceOfTask
+    {
+        Important = 1,
+        MediumImportant,
+        NotImportant
+    }
+
+    public enum Status
+    {
+        NotDone,
+        InProgress,
+        Done
+    }
+
+    public enum Type
+    {
+        Work,
+        Personal,
+        Hobby
+    }
+
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descrition { get; set; }
+        public ImporanceOfTask ImporanceOfTask { get; set; }
+        public Status Status { get; set; }
+        public Type Type { get; set; }
+        public List<SubTask> SubTasks { get; set; } = new List<SubTask>();
+    }
+
+
+}
